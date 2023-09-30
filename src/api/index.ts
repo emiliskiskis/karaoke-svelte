eb9310d80de9722e0cb4723543be1ae4a5292a5a
Emilis Kiškis <emiliskiskis@protonmail.com>
2023-10-01 01:37:14 +0300

    random files

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,22 @@
-import { MongoClient } from 'mongodb';
+import { Collection, MongoClient } from 'mongodb';
 import type { Song, SongMeta } from './types';
 
 export const noId = { projection: { _id: 0 } };
 
-const client = new MongoClient('mongodb://localhost:27017');
-export const songsCollection = client.db('local').collection('songs');
+export const query = async <T>(q: (collection: Collection<Song>) => T) => {
+	const client = new MongoClient('mongodb://localhost:27017');
+	try {
+		const collection = client.db('local').collection<Song>('songs');
+		return await q(collection);
+	} finally {
+		await client.close();
+	}
+};
 
 export const getSongs = () =>
-	songsCollection.find<Song>({}).project<SongMeta>({ _id: 0, title: 1, videoId: 1 }).toArray();
+	query((collection) =>
+		collection.find({}).project<SongMeta>({ _id: 0, title: 1, videoId: 1 }).toArray()
+	);
 
-export const getSong = (id: string) => songsCollection.findOne<Song>({ videoId: id }, noId);
+export const getSong = (id: string) =>
+	query((collection) => collection.findOne<Song>({ videoId: id }, noId));
